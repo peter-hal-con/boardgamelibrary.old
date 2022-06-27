6c893473f94160ea5ae3f4159989e8b02e5267d7
Peter-Frank Spierenburg <peter@hal-con.com>
2022-06-27 01:29:04 -0300

    Copy instances have uuids

--- server/grails-app/domain/com/halcon/gaming/boardgamelibrary/Copy.groovy
+++ server/grails-app/domain/com/halcon/gaming/boardgamelibrary/Copy.groovy
@@ -1,7 +1,16 @@
 package com.halcon.gaming.boardgamelibrary
 
 class Copy {
+    String uuid = UUID.randomUUID().toString()
 
     static constraints = {
+        uuid nullable:false, validator: {
+            try {
+                return UUID.fromString(it).toString().equals(it)
+            } catch (IllegalArgumentException e) {
+                return false
+            }
+            return true
+        }
     }
 }
--- server/src/test/groovy/com/halcon/gaming/boardgamelibrary/CopySpec.groovy
+++ server/src/test/groovy/com/halcon/gaming/boardgamelibrary/CopySpec.groovy
@@ -11,8 +11,28 @@ class CopySpec extends Specification implements DomainUnitTest<Copy> {
     def cleanup() {
     }
 
-    void "test something"() {
-//        expect:"fix me"
-//            true == false
+    void "test a Copy may not have a null uuid"() {
+        expect:
+        false == new Copy(uuid:null).validate()
+    }
+
+    void "test a Copy may have a valid uuid"() {
+        expect:
+        true == new Copy(uuid:"a49150b4-087d-4eb3-895a-2bb351351637").validate()
+    }
+
+    void "test a Copy may not have an invalid uuid"() {
+        expect:
+        false == new Copy(uuid:"not a uuid").validate()
+    }
+
+    void "test a Copy with no uuid will be given one at construction time"() {
+        expect:
+        null != new Copy().uuid
+    }
+
+    void "test a Copy that is given a uuid will keep it"() {
+        expect:
+        "a49150b4-087d-4eb3-895a-2bb351351637" == new Copy(uuid:"a49150b4-087d-4eb3-895a-2bb351351637").uuid
     }
 }
